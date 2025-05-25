cf779983105fc01bb57a0756a051dd1a73df0c1e
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-25 11:36:47 +0600

    header section done successfully

--- my-project/src/Components/MainLayout/Header/Header.jsx
+++ my-project/src/Components/MainLayout/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="navbar bg-base-100 lg:px-20">
+        <div className="navbar bg-base-100 w-9/12 mx-auto">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
