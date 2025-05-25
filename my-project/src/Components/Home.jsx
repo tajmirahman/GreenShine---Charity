b368aeb8cae6bc2ba5aab101bb24d6208a581602
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-25 10:44:19 +0600

    charity data json is updated

--- my-project/src/Components/Home.jsx
+++ my-project/src/Components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navber from './Navber';
+import OurCauses from './OurCauses';
 
 const Home = () => {
     return (
         <div>
             <Navber></Navber>
+            {/* Our causes */}
+            <OurCauses></OurCauses>
         </div>
     );
 };
---
+++ my-project/src/Components/OurCauses.jsx
@@ -0,0 +1,23 @@
+
+
+const OurCauses = () => {
+    return (
+        <div className="">
+            <div className="my-10 space-y-3">
+                <h1 className="text-4xl  text-center">OUR <span className="text-[#00A9A5]">CAUSES</span></h1>
+
+                <div class="flex items-center justify-center w-1/6 mx-auto">
+                    <div class="flex-grow h-px bg-gray-300"></div>
+                    <div class="mx-4 border border-gray-300 p-1 rounded-sm">
+                        <div class="w-1 h-1 border border-gray-400"></div>
+                    </div>
+                    <div class="flex-grow h-px bg-gray-300"></div>
+                </div>
+
+                <p className="text-center">Our charity help those people who have no hope</p>
+            </div>
+        </div>
+    );
+};
+
+export default OurCauses;
