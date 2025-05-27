6dc104f7e6293220fc8a913463cb5ca68ebe3a8e
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-28 01:28:52 +0600

    gallery data is fetch

--- my-project/src/Components/Gallary.jsx
+++ my-project/src/Components/Gallary.jsx
@@ -4,7 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 const Gallary = () => {
 
     const categories=useLoaderData();
-    console.log(data)
+
 
     return (
         <div className='my-6'>
@@ -20,7 +20,11 @@ const Gallary = () => {
                 <p className="w-3/6 text-center mx-auto">We create events aiming to pear to the voice for children and gather for support. Please update with our events and confirm you presence.</p>
             </div>
 
-
+            <div className='flex flex-row justify-center items-center gap-3 mt-6'>
+                {
+                    categories.map(category=> <button className='btn bg-[#00A9A5] text-white'>{category.category}</button>)
+                }
+            </div>
 
         </div>
     );
