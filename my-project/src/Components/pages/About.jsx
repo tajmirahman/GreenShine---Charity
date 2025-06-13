2d9ecd89d53e93d594d5647fe1af0bad17904437
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-06-13 15:52:39 +0600

    about us image work is processing

--- my-project/src/Components/pages/About.jsx
+++ my-project/src/Components/pages/About.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import aboutUs from '../../assets/image/about_us.webp'
 import Header from '../MainLayout/Header/Header';
 
 const About = () => {
     return (
         <div>
             <Header></Header>
+
+            <div className="h-[300px] bg-cover bg-center flex flex-col justify-center items-center"
+                style={{
+                    backgroundImage:
+                        `url(${aboutUs})`,
+                }}>
+
+                <div >
+                    <Link to={'/'}><h1 className='uppercase text-3xl font-bold text-white'>Abouts Us</h1></Link>
+                    <p className='text-center'>Home / <span className=" font-bold">About Us</span></p>
+                </div>
+            </div>
         </div>
     );
 };
