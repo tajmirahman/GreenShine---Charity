017d7d65035788dabb634bc02d19a4cb2117e1c3
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-06-13 12:16:54 +0600

    help us page problem solving done

--- my-project/src/Components/HelpUs.jsx
+++ my-project/src/Components/HelpUs.jsx
@@ -1,7 +1,12 @@
 
-import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const HelpUs = () => {
+
+    const [fullDonator,setFullDonator]=useState(false);
+    const [showTextFur,setShowTextFur]=useState(false);
+    const [showTextVol,setShowTextVol]=useState(false);
 
 
     const fullTextDonator = `Becoming a donor is one of the most impactful ways to support our mission. Your generous financial contributions directly help us provide essential resources such as food, shelter, education, and medical care to those who need them most. Every donation, big or small, plays a vital role in changing lives and bringing hope to underserved communities. With your support, we can expand our outreach, respond quickly to emergencies, and continue running our programs that aim to break the cycle of poverty and injustice. As a donor, you become part of a compassionate network of individuals committed to making a difference. We ensure full transparency in how funds are used, and we regularly share updates so you can see the real-world impact of your giving. Join us in transforming lives—your donation can be the spark of hope someone has been waiting for.`;
@@ -32,18 +37,18 @@ const HelpUs = () => {
             <div className='md:flex justify-center items-center w-4/6 mx-auto mt-10 gap-3 '>
                 <div className='space-y-3'>
                     <h1 className='text-3xl uppercase'>Donator</h1>
-                    <p className='text-white'>{previewTextDonator}</p>
-                    <Link to={'/'} ><button className='border-2 border-white py-2 px-2 mt-2'>Learn more</button></Link>
+                    <p className='text-white'>{fullDonator ? fullTextDonator: previewTextDonator}</p>
+                    <button onClick={()=>setFullDonator(!fullDonator)} className='border-2 border-white py-2 px-2 mt-2'> {fullDonator ? 'Show less' : 'Learn More'}</button>
                 </div>
                 <div className='space-y-3'>
                     <h1 className='text-3xl uppercase'>Funrising</h1>
-                    <p className='text-white'>{previewTextFur}</p>
-                   <Link to={'/'} ><button className='border-2 border-white py-2 px-2 mt-2'>Learn more</button></Link>
+                    <p className='text-white'>{showTextFur? fullTextFur : previewTextFur}</p>
+                   <Link to={'/'} ><button onClick={()=>{setShowTextFur (!showTextFur)}} className='border-2 border-white py-2 px-2 mt-2'>{showTextFur ? 'Show less' : 'Learn More'}</button></Link>
                 </div>
                 <div className='space-y-3 '>
                     <h1 className='text-2xl uppercase'>Volunteer</h1>
-                    <p className='text-white'>{ previewTextVol}</p>
-                    <Link to={'/'} ><button className='border-2 border-white py-2 px-2 mt-2'>Learn more</button></Link>
+                    <p className='text-white'>{showTextVol ? fullTextVol: previewTextVol}</p>
+                    <Link to={'/'} ><button onClick={()=>setShowTextVol(!showTextVol)} className='border-2 border-white py-2 px-2 mt-2'>{showTextVol ? 'Show less' : 'Learn More'}</button></Link>
                 </div>
             </div>
         </div>
